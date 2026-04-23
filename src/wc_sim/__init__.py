71f3cd7e2f7cbc8b9855b953edfff012c61a6302
Emiliano Santacruz <mxesco@byu.edu>
2026-04-22 18:59:32 -0600

    Add compare_rank_changes and tune_weight to __all__

--- src/wc_sim/__init__.py
+++ src/wc_sim/__init__.py
@@ -1,5 +1,12 @@
-from .wrangling import calculate_weighted_rating
+from .wrangling import calculate_weighted_rating, compare_rank_changes
 from .simulation import simulate_match, simulate_match_n
-from .validation import validate_weighted_rating
+from .validation import validate_weighted_rating, tune_weight
 
-__all__ = ["calculate_weighted_rating", "simulate_match", "simulate_match_n", "validate_weighted_rating"]
+__all__ = [
+    "calculate_weighted_rating",
+    "compare_rank_changes",
+    "simulate_match",
+    "simulate_match_n",
+    "validate_weighted_rating",
+    "tune_weight",
+]
